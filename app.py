aa3d1baee9ce93f89dd557354660ed3d68099d45
Jeannaej <jeannaejy22@gmail.com>
2021-06-10 20:49:25 -0500

    Fixed the index route

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # pickleScaler = load(open('',''))
 
 # Define the index route
-@app.route('/index.html')
+@app.route('/')
 def home():
     return render_template('index.html')
 
