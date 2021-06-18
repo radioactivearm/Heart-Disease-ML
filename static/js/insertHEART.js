3aa78ab4d5aea584138ab39da5ea21d1532a753a
arm <andy.mcrae21@gmail.com>
2021-06-17 20:24:43 -0500

    added jane

--- static/js/insertHEART.js
+++ static/js/insertHEART.js
@@ -1,29 +1,19 @@
 console.log('loaded insertHEART.js');
 
 
-// const patients = [
-//     [57, 1, 2, 124, 261, 0, 0, 141, 0, 0.3, 1],
-//     [43, 1, 4, 115, 303, 0, 0, 181, 0, 1.2, 2],
-//     [42, 0, 3, 120, 209, 0, 0, 173, 0, 0.0, 2],
-//     [67, 1, 4, 120, 237, 0, 0, 71, 0, 1.0, 2],
-//     [71, 0, 2, 160, 302, 0, 0, 162, 0, 0.4, 1]
-// ];
+const jane = [32, 0, 2, 115, 228, 1, 1, 80, 0, 0, 1];
 
-// function insertHEART() {
+function insertJane() {
 
-//         // https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
-//         let patient = patients[Math.floor(Math.random()*patients.length)];
+        let ids = ['id1', 'id2', 'id3', 'id4', 'id5', 'id6', 'id7', 'id8', 'id9', 'id10', 'id11'];
 
-//         let ids = ['id1', 'id2', 'id3', 'id4', 'id5', 'id6', 'id7', 'id8', 'id9', 'id10', 'id11'];
+        const len = jane.length;
 
-//         const len = patient.length;
+        for (let i = 0; i < len; i++) {
+            document.getElementById(ids[i]).value = jane[i];
+        }
 
-//         for (let i = 0; i < len; i++) {
-//             document.getElementById(ids[i]).value = patient[i];
-//         }
-
-
-// }
+}
 
 
 function insertHEART() {
@@ -33,12 +23,17 @@ function insertHEART() {
     let clinic = [];
 
 
-    d3.csv('/static/data/cut.csv', patients => {
+    d3.csv('/static/data/cut.csv').then(patients => {
 
         // console.log(patients.length);
 
-        clinic.push([patients.age, patients.sex, patients['chest pain type'], patients['resting bp s'], patients.cholesterol, patients['fasting blood sugar'],
-                patients['resting ecg'], patients['max heart rate'], patients['exercise angina'], patients.oldpeak, patients['ST slope']]);
+        // clinic.push([patients.age, patients.sex, patients['chest pain type'], patients['resting bp s'], patients.cholesterol, patients['fasting blood sugar'],
+        //         patients['resting ecg'], patients['max heart rate'], patients['exercise angina'], patients.oldpeak, patients['ST slope']]);
+
+        patients.forEach(patient => {
+            clinic.push([patient.age, patient.sex, patient['chest pain type'], patient['resting bp s'], patient.cholesterol, patient['fasting blood sugar'],
+                    patient['resting ecg'], patient['max heart rate'], patient['exercise angina'], patient.oldpeak, patient['ST slope']]);
+        });
         
         // https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
         let patient = clinic[Math.floor(Math.random()*clinic.length)];
